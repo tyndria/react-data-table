15a1c921cb65a3e1b4207e7dc45bfce8fab9e085
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2017-08-01 21:34:42 +0300

    Clean up data

--- src/data/index.js
+++ src/data/index.js
@@ -1,192 +1,165 @@
 const records = [
 	{
-		musician: 'Ascar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
+		musician: 'Art Tatum',
+		year: '1930',
 		song: 'When I was young',
 		genre: 'Jazz'
 	},
 	{
-		musician: 'Jason Mraz',
-		album: 'best',
-		year: '2010',
+		musician: 'Betty Carter',
+		year: '1956',
 		song: 'Plain',
-		genre: 'Pop'
+		genre: 'Jazz'
 	},
 	{
-		musician: 'Tommy Emmanuel',
-		album: 'Christmas',
+		musician: 'Capital Lights',
 		year: '2005',
 		song: 'Family',
-		genre: 'Instrumental'
+		genre: 'Rock'
 	},
 	{
-		musician: 'Bscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
+		musician: 'Pocket Full of Rocks',
+		year: '2000',
+		song: 'Want you',
+		genre: 'Rock'
 	},
 	{
-		musician: 'Cscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
+		musician: 'Echo Orbiter',
+		year: '1999',
+		song: 'Home',
+		genre: 'Indie-pop'
 	},
 	{
-		musician: 'Iscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
+		musician: 'Dan Smith',
+		year: '2010',
+		song: 'Never mind',
+		genre: 'Indie'
 	},
 	{
-		musician: 'Fscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
+		musician: 'Sergio Vargas',
+		year: '1997',
+		song: 'Jumpy',
+		genre: 'Merengue'
 	},
 	{
-		musician: 'Gscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
+		musician: 'Krisspy',
+		year: '1967',
+		song: 'Forgive',
+		genre: 'Merengue'
 	},
 	{
-		musician: 'Hscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
+		musician: 'Great Big Sea',
+		year: '2000',
+		song: 'Louder',
+		genre: 'Folk'
 	},
 	{
-		musician: 'Iscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
+		musician: 'Harmonium',
+		year: '1970',
+		song: 'Cross over',
+		genre: 'Folk'
 	},
 	{
 		musician: 'Jscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Kscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Lscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Mscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Nscar Peterson',
-		album: 'Bossa Nova',
 		year: '1978',
 		song: 'When I was young',
 		genre: 'Jazz'
 	},
 	{
 		musician: 'Oscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Pscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Qscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Rscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Sscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Tscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Uscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Vscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Xscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Yscar Peterson',
-		album: 'Bossa Nova',
-		year: '1978',
-		song: 'When I was young',
-		genre: 'Jazz'
-	},
-	{
-		musician: 'Zscar Peterson',
-		album: 'Bossa Nova',
 		year: '1978',
 		song: 'When I was young',
 		genre: 'Jazz'
 	},
 	{
 		musician: 'Oscar Peterson',
-		album: 'Bossa Nova',
+		year: '1980',
+		song: 'Summer',
+		genre: 'Jazz'
+	},
+	{
+		musician: 'A Day to Remember',
+		year: '2005',
+		song: 'Hidden',
+		genre: 'Emo'
+	},
+	{
+		musician: 'Fall Out Boy',
+		year: '2003',
+		song: 'Growing',
+		genre: 'Emo'
+	},
+	{
+		musician: 'Oscar Peterson',
 		year: '1978',
 		song: 'When I was young',
 		genre: 'Jazz'
+	},
+	{
+		musician: 'Larnelle Harris',
+		year: '1980',
+		song: 'Mother',
+		genre: 'Gospel'
+	},
+	{
+		musician: 'Inez Andrews',
+		year: '2012',
+		song: 'Sunshine',
+		genre: 'Gospel'
+	},
+	{
+		musician: 'Ira D. Sankey',
+		year: '1995',
+		song: 'God',
+		genre: 'Christianity'
+	},
+	{
+		musician: 'Lawrence Welk',
+		year: '1990',
+		song: 'Lola',
+		genre: 'Polka'
+	},
+	{
+		musician: 'Myron Floren',
+		year: '2004',
+		song: 'Jija',
+		genre: 'Polka'
+	},
+	{
+		musician: 'Molotov',
+		year: '2006',
+		song: 'Hanging',
+		genre: 'Latin'
+	},
+	{
+		musician: 'Da Lata',
+		year: '1994',
+		song: 'Pray for you',
+		genre: 'Latin'
+	},
+	{
+		musician: 'Cecil Brower',
+		year: '1995',
+		song: 'Rock step',
+		genre: 'Swing'
+	},
+	{
+		musician: 'Orgy',
+		year: '2003',
+		song: 'Whisper',
+		genre: 'Metal'
+	},
+	{
+		musician: 'Godhead',
+		year: '1986',
+		song: 'Blue eyes',
+		genre: 'Metal'
+	},
+	{
+		musician: 'Jimmy Dorsey ',
+		year: '1984',
+		song: 'Ha ha la',
+		genre: 'Swing'
 	},
 ];
 
